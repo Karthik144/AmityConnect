19bd43dedffb659a08a072e71f3836ed57a545bc
lizardon <chia.qien.don@dhs.sg>
2021-12-03 21:08:42 +0800

    test

--- AmityConnect/ViewController.swift
+++ AmityConnect/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // test, trying to use source control through xcode, dont mind me
     }
 
 
