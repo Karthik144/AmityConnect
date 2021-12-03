f39a7bb3e40a163b35c6883847673b364b4e7ddd
Karthik Ramu <karthik.k.ramu@gmail.com>
2021-12-03 00:35:05 -0500

    Initial Commit

---
+++ AmityConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmityConnect
+//
+//  Created by Karthik  Ramu on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
