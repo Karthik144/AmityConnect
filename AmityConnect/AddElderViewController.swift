5a6150f4a0be5b8ebb3c3321d20e6de694465ee0
“Karthik144” <karthik.k.ramu@gmail.com>
2021-12-21 23:16:24 -0500

    Started backend integration

---
+++ AmityConnect/AddElderViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddElderViewController.swift
+//  AmityConnect
+//
+//  Created by Karthik  Ramu on 12/21/21.
+//
+
+import UIKit
+import Firebase
+
+class AddElderViewController: UITableViewController {
+    
+    // IB Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var healthTextField: UITextField!
+    @IBOutlet weak var careTakerTextField: UITextField!
+    @IBOutlet weak var familyEmailTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    let db = Firestore.firestore()
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        
+        
+        db.collection("centers").document("Wo5A6ujH3jhPUfWnaIkI").collection("center_elders").document("6Hi9FbYWZr6c0reCarng").setData(["age": ageTextField.text ?? "", "condition": healthTextField.text ?? "none", "name": nameTextField.text ?? "", "caretaker": careTakerTextField.text ?? "", "family_email": familyEmailTextField.text ?? ""])
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AmityConnect/View/HomeViewController.swift
+++ AmityConnect/View/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UITableViewController {
 
@@ -15,15 +16,14 @@ class HomeViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    let db = Firestore.firestore()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addElderButton(_ sender: UIBarButtonItem) {
+        
+        
+     
+        
     }
-    */
-
+    
 }
