84fa8238c59ef9c651ad4a5a0099f217738a8f89
lizardon <chia.qien.don@dhs.sg>
2021-12-23 17:49:48 +0800

    fixed elderly view, search bar margin and a bunch of other alignments

--- AmityConnect/Controller/HomeViewController.swift
+++ AmityConnect/Controller/HomeViewController.swift
@@ -25,7 +25,7 @@ class HomeViewController: UITableViewController, UISearchBarDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        searchBar.delegate = self
+//        searchBar.delegate = self
         eldersCollectionRef = db.collection("centers").document("Wo5A6ujH3jhPUfWnaIkI").collection("center_elders")
         
         elderNames = []
